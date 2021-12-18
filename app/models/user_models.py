99d3931927c51405797e93865c7ca36f617683c9
Fred Zimmerman <fredzannarbor@gmail.com>
2021-12-17 23:05:08 -0500

    working on flask

--- app/models/user_models.py
+++ app/models/user_models.py
@@ -1,7 +1,7 @@
 # Copyright 2014 SolidBuilds.com. All rights reserved
 #
 # Authors: Ling Thio <ling.thio@gmail.com>
-
+from flask import Flask, redirect, render_template, request, url_for
 from flask_user import UserMixin
 # from flask_user.forms import RegisterForm
 from flask_wtf import FlaskForm
